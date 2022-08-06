e66e33d0e3b7c706475a7a8d4d1d25ee7a47d567
rahul31agrawal <rahul31agrawal@gmail.com>
2022-08-06 22:43:13 +0530

    Feat: greeting weather & todo feature added

--- src/Component/Greeting/Greeting.js
+++ src/Component/Greeting/Greeting.js
@@ -1,13 +1,47 @@
 import React, { useEffect, useState } from "react";
 import "../Time/Time.css";
+import "./Greeting.css";
 
 function Greeting() {
-  const [greetingMessage, setGreetingMessage] = useState("");
-  const [userName, setuserName] = useState("");
+  const [greetingMessage, setGreetingMessage] = useState('');
+  const [userName, setUserName] = useState('');
+  const [isMade, setIsMade] = useState(true);
+
+  const submitUserName = (e) => {
+    e.preventDefault();
+    console.log(userName);
+    setIsMade(true)
+  }
+
+  let name;
+  if (isMade) {
+    name = (
+      <span>{userName}</span>
+    )
+  }
+
+  const openNameInput = () => {
+    setIsMade(false);
+  }
+
+  useEffect(() => {
+    const item = localStorage.getItem("userName");
+    const loadedItem = JSON.parse(item);
+    if (loadedItem) {
+      setUserName(loadedItem);
+    }
+  }, []);
+
+  useEffect(() => {
+    const json = JSON.stringify(userName);
+    localStorage.setItem("userName", json);
+  }, [userName])
+
 
   useEffect(() => {
     let d = new Date();
     let h = d.getHours();
+
     if (h > 4 && h < 12) {
       setGreetingMessage("Good Morning");
     } else if (h > 12 && h < 17) {
@@ -15,18 +49,31 @@ function Greeting() {
     } else {
       setGreetingMessage("Good Evening");
     }
-  }, []);
-
+  }, [])
 
   return (
     <div className="time-card greeting">
       <span>{greetingMessage}</span>
       <br />
-      <form>
-        <input type="text" 
-        onChange={(e)=>setuserName(e.target.value)}
-        />
-      </form>
+      {
+        !isMade ?
+          <form
+            className="greeting-form"
+            onSubmit={submitUserName}>
+            <input
+              type="text"
+              value={userName}
+              onChange={event => setUserName(event.target.value)}
+            />
+          </form>
+          :
+          <span
+            className="user-name"
+            onClick={openNameInput}
+          >
+            {name}!
+          </span>
+      }
     </div>
   );
 }
