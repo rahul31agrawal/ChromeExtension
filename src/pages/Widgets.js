cd8816026f3f8b5aa4f2477238dd78f3a1829876
rahul31agrawal <rahul31agrawal@gmail.com>
2022-08-10 09:51:31 +0530

    Feat: Added background wallpaper

--- src/pages/Widgets.js
+++ src/pages/Widgets.js
@@ -1,34 +1,35 @@
 import React from "react";
 import "./Widget.css";
 
-import Todo from '../Component/Todo/Todo';
-import Greeting from '../Component/Greeting/Greeting';
-import Time from '../Component/Time/Time';
-import Weather from '../Component/Weather/Weather';
-import Quotes from '../Component/Quotes/Quotes';
-
+import Todo from "../Component/Todo/Todo";
+import Greeting from "../Component/Greeting/Greeting";
+import Time from "../Component/Time/Time";
+import Weather from "../Component/Weather/Weather";
+import Quotes from "../Component/Quotes/Quotes";
 
 function Widgets() {
   return (
-    <div className="widgetContainer">
-      <div className="item2">
-        <Time />
-      </div>
+    <div className="wallpaper">
+      <div className="widgetContainer">
+        <div className="item2">
+          <Time />
+        </div>
 
-      <div className="item3">
-        <Greeting />
-      </div>
+        <div className="item3">
+          <Greeting />
+        </div>
 
-      <div className="item4">
-        <Weather />
-      </div>
+        <div className="item4">
+          <Weather />
+        </div>
 
-      <div className="item5">
-        <Todo />
-      </div>
+        <div className="item5">
+          <Todo />
+        </div>
 
-      <div className="item6">
-        <Quotes />
+        <div className="item6">
+          <Quotes />
+        </div>
       </div>
     </div>
   );
