31ba264b49933a0235c8cc71f7ae3c0270ef0805
rahul31agrawal <rahul31agrawal@gmail.com>
2022-08-21 18:09:37 +0530

    Feat: Added focus feature
    I have added Focus Feature & fixed few bugs

--- src/Component/OnboardUser/OnboardUser.js
+++ src/Component/OnboardUser/OnboardUser.js
@@ -1,105 +1,106 @@
-import React, { useEffect, useState} from "react";
-import {useUserName} from "../../context/userContex";
-import { useNavigate } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { useUserName } from "../../context/userContex";
+// import { useNavigate } from 'react-router-dom';
+import Widgets from "../../pages/Widgets";
+import { Link } from "react-chrome-extension-router";
 import "../Time/Time.css";
 import "./Onboarduser.css";
 
-
-
 function OnboardUser() {
-  
+  const [greetingMessage, setGreetingMessage] = useState("");
 
- 
+  // const [isMade, setIsMade] = useState(true);
 
-    const [greetingMessage, setGreetingMessage] = useState('');
-  
-    const [isMade, setIsMade] = useState(true);
-  
-    const {userName, setUserName} = useUserName();
-    const { setTodos} =useUserName([]);
-    let navigate = useNavigate();
+  const { userName, setUserName,userFocus, setUserFocus } = useUserName();
 
-  
-    const submitUserName = (e) => {
-      e.preventDefault();
-      console.log(userName);
-      setIsMade(true)
-      navigate('/widgets');
-      setTodos([])
+  const { setTodos } = useUserName([]);
+  // let navigate = useNavigate();
+
+  const submitUserName = (e) => {
+    e.preventDefault();
+    console.log(userName);
+    // setIsMade(true)
+    // navigate('/widgets');
+    setTodos([]);
+  };
+
+  // let name;
+  // if (isMade) {
+  //   name = (
+  //     <span>{userName}</span>
+  //   )
+  // }
+
+  // const openNameInput = () => {
+  //   setIsMade(false);
+  // }
+
+  useEffect(() => {
+    const item = localStorage.getItem("userName");
+    const loadedItem = JSON.parse(item);
+
+    // const itemFocus = localStorage.getItem("userFocus");
+    // const loadedFocus = JSON.parse(itemFocus);
+
+    if (loadedItem  ) {
+      setUserName(loadedItem);
+      // setUserFocus(loadedFocus);
     }
-  
-    let name;
-    if (isMade) {
-      name = (
-        <span>{userName}</span>
-      )
+  }, [setUserName]);
+
+  useEffect(() => {
+    const json = JSON.stringify(userName);
+    localStorage.setItem("userName", json);
+
+    // const json2 = JSON.stringify(userFocus);
+    // localStorage.setItem("userFocus", json2);
+
+  }, [userName]);
+
+  useEffect(() => {
+    let d = new Date();
+    let h = d.getHours();
+
+    if (h > 4 && h < 12) {
+      setGreetingMessage("Good Morning");
+    } else if (h > 12 && h < 17) {
+      setGreetingMessage("Good Afternoon");
+    } else {
+      setGreetingMessage("Good Evening");
     }
-  
-    const openNameInput = () => {
-      setIsMade(false);
-    }
-  
-    useEffect(() => {
-      const item = localStorage.getItem("userName");
-      const loadedItem = JSON.parse(item);
-      if (loadedItem) {
-        setUserName(loadedItem);
+  }, []);
+
+  return (
+    <div className="onboard-card greeting">
+      <span>{greetingMessage}</span>
+      <br />
+      {
+        <div>
+          <form className="greeting-form">
+            <input
+              placeholder="Type name here"
+              type="text"
+              value={userName}
+              onChange={(event) => setUserName(event.target.value)}
+            />
+            <h3>What is your main focus for today ?</h3>
+
+            <input
+            placeholder="Enter your focus here"
+            type="text"
+              value={userFocus}
+              onChange={(event) => setUserFocus(event.target.value)}
+            />
+          </form>
+          <button className="submitBtn" onClick={submitUserName}>
+            <Link className="link" component={Widgets}>Submit</Link>
+          </button>
+        </div>
       }
-    }, [setUserName]);
-
-
-    
-  
-    useEffect(() => {
-      const json = JSON.stringify(userName);
-      localStorage.setItem("userName", json);
-    }, [userName])
-  
-  
-    useEffect(() => {
-      let d = new Date();
-      let h = d.getHours();
-  
-      if (h > 4 && h < 12) {
-        setGreetingMessage("Good Morning");
-      } else if (h > 12 && h < 17) {
-        setGreetingMessage("Good Afternoon");
-      } else {
-        setGreetingMessage("Good Evening");
-      }
-    }, [])
-  
-    return (
-      <div className="onboard-card greeting">
-        <span>{greetingMessage},  Please enter your name</span>
-        <br />
-        {
-          !isMade ?
-            <form
-              className="greeting-form"
-              onSubmit={submitUserName}>
-              <input
-             
-               placeholder="Enter name"
-                type="text"
-                value={userName}
-                onChange={event => setUserName(event.target.value)}
-              />
-            </form>
-            :
-            <span
-            
-              className="user-name"
-              onClick={openNameInput}
-            >
-              {name}!
-  
-              
-            </span>
-        }
-      </div>
-    );
+    </div>
+  );
 }
 
-export default OnboardUser
+export default OnboardUser;
+
+
--- src/pages/Widgets.js
+++ src/pages/Widgets.js
@@ -1,16 +1,24 @@
 import React from "react";
 import "./Widget.css";
-
+import {Link} from "react-chrome-extension-router";
 import Todo from "../Component/Todo/Todo";
 import Greeting from "../Component/Greeting/Greeting";
 import Time from "../Component/Time/Time";
 import Weather from "../Component/Weather/Weather";
 import Quotes from "../Component/Quotes/Quotes";
-
+import Home from "../pages/home/Home";
+import FocusComp from "../Component/focus/FocusComp";
 function Widgets() {
   return (
     <div className="wallpaper">
+      <div className="backBtnDiv">
+        <button className="backBtn"><Link className="link" component={Home}>Back to Home</Link></button>
+      </div>
       <div className="widgetContainer">
+        <div className="item1">
+          <FocusComp/>
+        </div>
+
         <div className="item2">
           <Time />
         </div>
